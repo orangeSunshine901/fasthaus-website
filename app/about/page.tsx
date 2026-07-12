6bbd5a7536881cc79412c17515f1d584a6ab84e4
Abdul Latif <latif901@hotmail.com>
2026-07-12 19:08:01 +0400

    broweser issue test for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,10 @@
 import AboutScrollStory from "@/components/about/AboutScrollStory";
 import ShopLayout from "@/components/layout/ShopLayout";
-import LocomotiveScrollProvider from "@/components/scroll/LocomotiveScrollProvider";
 
 export default function AboutPage() {
   return (
-    <LocomotiveScrollProvider>
-      <ShopLayout showAnnouncement={false}>
-        <AboutScrollStory />
-      </ShopLayout>
-    </LocomotiveScrollProvider>
+    <ShopLayout showAnnouncement={false}>
+      <AboutScrollStory />
+    </ShopLayout>
   );
 }
